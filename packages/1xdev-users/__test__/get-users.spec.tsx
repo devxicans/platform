5fdbdcd9d0763f9c26b67aba6b0728ce40618019
juanpaceja <143227440+juanpaceja@users.noreply.github.com>
2024-12-11 15:31:21 -0600

    adding pablo

--- packages/1xdev-users/__test__/get-users.spec.tsx
+++ packages/1xdev-users/__test__/get-users.spec.tsx
@@ -4,6 +4,7 @@ describe('getUsers', () => {
   it('Should get users', () => {
     const users = getUsers();
     expect(users).toHaveLength(3);
+    expect(users[3]).toEqual('pablo')
     expect(users).toContain('nava')
   });
 });
