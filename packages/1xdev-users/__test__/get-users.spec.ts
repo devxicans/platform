027a3b4680c4fdcc30f1462e89b3020f414ad648
c0v4m3rl0 <acovarrubias17@ucol.mx>
2024-12-11 15:01:12 -0600

    feat:new_change

--- packages/1xdev-users/__test__/get-users.spec.ts
+++ packages/1xdev-users/__test__/get-users.spec.ts
@@ -3,7 +3,8 @@ import { getUsers } from '../src';
 describe('getUsers', () => {
   it('Should get users', () => {
     const users = getUsers();
-    expect(users).toHaveLength(3);
-    expect(users).toContain('nava')
+    expect(users).toHaveLength(4);
+    expect(users).toContain('nava'),
+    expect(users).toContain('Adrian')
   });
 });
