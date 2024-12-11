0618e0cec0b312d462406f0048218380139b4cf8
juanpaceja <143227440+juanpaceja@users.noreply.github.com>
2024-12-11 15:36:11 -0600

    feat: making the app workds

--- packages/1xdev-users/__test__/get-users.spec.tsx
+++ packages/1xdev-users/__test__/get-users.spec.tsx
@@ -3,7 +3,7 @@ import { getUsers } from '../src';
 describe('getUsers', () => {
   it('Should get users', () => {
     const users = getUsers();
-    expect(users).toHaveLength(3);
+    expect(users).toHaveLength(4);
     expect(users[3]).toEqual('pablo')
     expect(users).toContain('nava')
   });
