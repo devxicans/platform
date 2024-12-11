bc11d158a2be1f956d0433acdd57b189050709bb
Felipe Nava <felipe.nava.co@gmail.com>
2024-12-10 21:55:36 -0800

    feat: adding unit test support (#4)

---
+++ jest-startup.ts
@@ -0,0 +1,19 @@
+import { format } from 'util';
+
+import 'jest-environment-jsdom';
+import '@testing-library/jest-dom';
+import '@testing-library/react';
+
+import { TextEncoder } from 'util';
+
+global.TextEncoder = TextEncoder;
+
+// eslint-disable-next-line no-undef
+console.error = function (...args) {
+  throw new Error(format(...args));
+};
+
+// eslint-disable-next-line no-undef
+console.warn = function (...args) {
+  throw new Error(format(...args));
+};
