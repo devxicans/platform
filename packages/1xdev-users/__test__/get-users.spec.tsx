cc5bb0031f27b42dcc0d2403b9200b0354cba3a5
juanpaceja <143227440+juanpaceja@users.noreply.github.com>
2024-12-11 15:50:27 -0600

    feat:adding my name to the users (#14)

--- packages/1xdev-users/__test__/get-users.spec.tsx
+++ packages/1xdev-users/__test__/get-users.spec.tsx
@@ -3,7 +3,8 @@ import { getUsers } from '../src';
 describe('getUsers', () => {
   it('Should get users', () => {
     const users = getUsers();
-    expect(users).toHaveLength(4);
+    expect(users).toHaveLength(5);
+    expect(users).toContain('pablo')
     expect(users).toContain('nava'),
     expect(users).toContain('Adrian')
   });
--- packages/1xdev-users/src/get-users.tsx
+++ packages/1xdev-users/src/get-users.tsx
@@ -1,3 +1,3 @@
 export const getUsers = () => {
-  return ['felipe', 'luis', 'nava', 'Adrian'];
+  return ['felipe', 'luis', 'nava', 'Adrian', 'pablo'];
 };
