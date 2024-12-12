6d658407d00af306166cfe6289725d0293cbb7c7
AdrifeCova <adrife2000@hotmail.com>
2024-12-12 05:59:50 -0600

    fix: installed dependencies

--- packages/1xdev-random-number/__test__/get-random-number.spec.tsx
+++ packages/1xdev-random-number/__test__/get-random-number.spec.tsx
@@ -1,6 +1,6 @@
 import { getRandomNumber } from '../src';
 
-describe('getUsers', () => {
+describe('getRandomNumber', () => {
   it('Should return a random number', () => {
     const getRandom = getRandomNumber(10);
     expect(getRandom.random).toBeLessThan(10);
