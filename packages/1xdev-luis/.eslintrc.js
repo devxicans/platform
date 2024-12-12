ccadc83d182ffa83d85ccabd8dde003e1ecaf4d8
Luis Eduardo Nava Covarrubias <155337047+iNavac03@users.noreply.github.com>
2024-12-12 08:10:48 -0600

    feat: created my package (#18)

---
+++ packages/1xdev-luis/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ["@1xdev/eslint-config/library.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+};
---
+++ packages/1xdev-luis/src/get-total.tsx
@@ -0,0 +1,4 @@
+export const getTotal = () => {
+  let users = ['felipe', 'luis', 'nava', 'Adrian', 'pablo'];
+  return users.length;
+};
