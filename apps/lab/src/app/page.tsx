2c0aa448930185cb519e043ea9ed2bf62c5aa889
AdrifeCova <adrife2000@hotmail.com>
2024-12-13 10:14:12 -0600

    feat: finished added my ui component to my application

--- apps/lab/src/app/page.tsx
+++ apps/lab/src/app/page.tsx
@@ -1,12 +1,15 @@
 import { ViewWrapper } from './components';
+import { Button } from '@1xdev/get-random-number'
 
 import styles from './home.module.css';
 
 export default function Page() {
+
   return (
     <ViewWrapper>
       <div className={styles.main}>
         <h1 className={styles.hero}>Tech lab</h1>
+        <Button type='button' className={styles.btn}>Hello</Button>
       </div>
     </ViewWrapper>
   );
