92e4de2d8afe023df9a65567c7c4296e7242a7b6
AdrifeCova <adrife2000@hotmail.com>
2024-12-16 16:18:44 -0600

    feat: added cypress test

--- apps/homepage/__tests__/e2e/test.cy.tsx
+++ apps/homepage/__tests__/e2e/test.cy.tsx
@@ -1,7 +1,16 @@
 describe('First test', () => {
-  it('Should check title', () => {
+  it('Should check Header see through out pages', () => {
     cy.visit('/');
 
     cy.findByText("Header").should('be.visible');
+
+    cy.visit('/projects');
+
+    cy.findByText("Header").should('be.visible');
+
+    cy.visit('/contact');
+
+    cy.findByText("Header").should('be.visible');
+
   });
 });
