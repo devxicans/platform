e040f2d534f26606594a09f189026f1af4c84fbc
Luis Eduardo Nava Covarrubias <155337047+iNavac03@users.noreply.github.com>
2024-12-17 11:28:07 -0600

    refactor: deleted my package and ui element from lab page (#30)

--- apps/lab/src/app/page.tsx
+++ apps/lab/src/app/page.tsx
@@ -1,6 +1,5 @@
 import { ViewWrapper } from './components';
 import { Button } from '@1xdev/get-random-number';
-import { CustomInput } from '@1xdev/luis';
 
 import styles from './home.module.css';
 
@@ -10,7 +9,6 @@ export default function Page() {
     <ViewWrapper>
       <div className={styles.main}>
         <h1 className={styles.hero}>Tech lab</h1>
-        <CustomInput label='My input' type='text' id='my-input' placeholder='Write here' name='My input'/>
         <Button type='button' className={styles.btn}>Click Here</Button>
       </div>
     </ViewWrapper>
