e4081e5e34cc3c3d08554f3597be8302fd100c30
Luis Eduardo Nava Covarrubias <155337047+iNavac03@users.noreply.github.com>
2024-12-17 11:28:07 -0600

    refactor: deleted my package and ui element from lab page (#30)

--- apps/lab/__tests__/e2e/test.cy.tsx
+++ apps/lab/__tests__/e2e/test.cy.tsx
@@ -10,16 +10,4 @@ describe('First test', () => {
 
     cy.findByRole('button', { name: 'Click Here' }).should('be.visible').click();
   })
-
-  it("Should check if label for input exists", () => {
-    cy.visit('/')
-
-    cy.findByRole('textbox', { name: 'My input' }).should('exist').and('be.visible');
-  })
-
-  it("Should check if input is writable", () => {
-    cy.visit('/')
-
-    cy.findByRole('textbox', { name: 'My input' }).type('Hola').should('have.value', 'Hola');
-  })
 });
