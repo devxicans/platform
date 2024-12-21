68a753705aa733703bb4b432561d8bb1d47f61e0
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2024-12-21 17:33:31 -0600

    test: added cypress tests for header and menu buttons

--- apps/homepage/__tests__/e2e/test.cy.tsx
+++ apps/homepage/__tests__/e2e/test.cy.tsx
@@ -1,12 +1,14 @@
 describe('First test', () => {
-  it('Should check Header see through out pages with lang en', () => {
+  it('Should check Header and buttons see through out pages with lang en', () => {
     cy.visit('/', {
       headers: {
         'Accept-Language': 'en'
       }
     });
-
-    cy.findByText("Header").should('be.visible');
+    cy.get('[data-testid="header"]').should('exist');
+    cy.findByRole('link', {name: 'Home'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Projects'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Contact'}).should('exist').and('be.visible');
 
     cy.visit('/projects', {
       headers: {
@@ -14,7 +16,10 @@ describe('First test', () => {
       }
     });
 
-    cy.findByText("Header").should('be.visible');
+    cy.get('[data-testid="header"]').should('exist');
+    cy.findByRole('link', {name: 'Home'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Projects'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Contact'}).should('exist').and('be.visible');
 
     cy.visit('/contact', {
       headers: {
@@ -22,17 +27,23 @@ describe('First test', () => {
       }
     });
 
-    cy.findByText("Header").should('be.visible');
+    cy.get('[data-testid="header"]').should('exist');
+    cy.findByRole('link', {name: 'Home'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Projects'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Contact'}).should('exist').and('be.visible');
   });
 
-  it('Should check Header see through out pages with lang es', () => {
+  it('Should check Header and buttons see through out pages with lang es', () => {
     cy.visit('/', {
       headers: {
         'Accept-Language': 'es'
       }
     });
 
-    cy.findByText("Encabezado").should('be.visible');
+    cy.get('[data-testid="header"]').should('exist');
+    cy.findByRole('link', {name: 'Inicio'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Proyectos'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Contacto'}).should('exist').and('be.visible');
 
     cy.visit('/projects', {
       headers: {
@@ -40,7 +51,10 @@ describe('First test', () => {
       }
     });
 
-    cy.findByText("Encabezado").should('be.visible');
+    cy.get('[data-testid="header"]').should('exist');
+    cy.findByRole('link', {name: 'Inicio'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Proyectos'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Contacto'}).should('exist').and('be.visible');
 
     cy.visit('/contact', {
       headers: {
@@ -48,6 +62,9 @@ describe('First test', () => {
       }
     });
 
-    cy.findByText("Encabezado").should('be.visible');
-  });
+    cy.get('[data-testid="header"]').should('exist');
+    cy.findByRole('link', {name: 'Inicio'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Proyectos'}).should('exist').and('be.visible');
+    cy.findByRole('link', {name: 'Contacto'}).should('exist').and('be.visible');
+    });
 });
