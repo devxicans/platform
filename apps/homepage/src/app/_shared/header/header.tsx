904dc98f89d3f61433292dae8945125ecb6f8f14
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2024-12-21 16:14:07 -0600

    feat: created header component with basic functionality

--- apps/homepage/src/app/_shared/header/header.tsx
+++ apps/homepage/src/app/_shared/header/header.tsx
@@ -1,5 +1,15 @@
-import './header.module.scss'
+import styles from './header.module.scss'
+import { MenuButton } from '../menuButton'
 
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <div className={styles.main}>
+      <h1>@1<span className={styles.spanText}>x</span>Dev</h1>
+      <div className={styles.buttonMenu}>
+      <MenuButton text='Home' href='/'/>
+      <MenuButton text='Projects' href='/projects'/>
+      <MenuButton text='Contact' href='/contact'/>
+      </div>
+    </div>
+  )
 } 
