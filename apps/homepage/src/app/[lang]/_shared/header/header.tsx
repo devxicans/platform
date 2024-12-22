8f433f8a4dc85266b13cdf6a6f47ebf7aeb46fc9
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2024-12-21 19:14:53 -0600

    feat: created the mobile menu

--- apps/homepage/src/app/[lang]/_shared/header/header.tsx
+++ apps/homepage/src/app/[lang]/_shared/header/header.tsx
@@ -1,21 +1,35 @@
-import styles from './header.module.scss'
-import { MenuButton } from '../menuButton'
+"use client";
+import styles from "./header.module.scss";
+import { MenuButton } from "../menuButton";
+import { MobileButton } from "../mobileButton";
+import { Breakpoints, UiViewport } from "@uireact/foundation";
 
 type HeaderProps = {
   loc: {
-    [x in string]: string
-  }
-}
+    [x in string]: string;
+  };
+};
 
 export function Header({ loc }: HeaderProps) {
   return (
-    <div className={styles.main} data-testid="header">
-      <h1>@1<span className={styles.spanText}>x</span>Dev</h1>
-      <div className={styles.buttonMenu}>
-      <MenuButton text={loc.menuBtn1} href='/'/>
-      <MenuButton text={loc.menuBtn2} href='/projects'/>
-      <MenuButton text={loc.menuBtn3} href='/contact'/>
-      </div>
-    </div>
-  )
+    <>
+      <UiViewport criteria="m|l|xl">
+        <div className={styles.main} data-testid="header">
+          <h1>@1<span className={styles.spanText}>x</span>Dev</h1>
+          <div className={styles.buttonMenu}>
+            <MenuButton text={loc.menuBtn1} href="/" />
+            <MenuButton text={loc.menuBtn2} href="/projects" />
+            <MenuButton text={loc.menuBtn3} href="/contact" />
+          </div>
+        </div>
+      </UiViewport>
+      <UiViewport criteria={Breakpoints.SMALL}>
+        <div className={styles.mobileMenu}>
+          <MobileButton icon="Home" href="/"/>
+          <MobileButton icon="Layers" href="/projects"/>
+          <MobileButton icon="Mail" href="/contact"/>
+        </div>
+      </UiViewport>
+    </>
+  );
 }
