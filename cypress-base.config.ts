c26440e85b5c921eef0bc124d2967202e8d21fc9
juanpaceja <143227440+juanpaceja@users.noreply.github.com>
2024-12-21 21:11:36 -0600

    fix: fixed file to put it as it was

--- cypress-base.config.ts
+++ cypress-base.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 const baseConfig = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: 'http://localhost:3000',
     supportFile: '../../cypress/support/e2e.js',
     supportFolder: '../../cypress/support',
     specPattern: '__tests__/e2e/**.cy.{ts,tsx}'
