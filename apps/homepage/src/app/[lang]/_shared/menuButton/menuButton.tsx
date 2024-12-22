c17daac3f66d7a2969cce8183839608c73fec266
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2024-12-21 18:21:01 -0600

    feat: added animation while hover on button

--- apps/homepage/src/app/[lang]/_shared/menuButton/menuButton.tsx
+++ apps/homepage/src/app/[lang]/_shared/menuButton/menuButton.tsx
@@ -1,15 +1,22 @@
-'use client'
-import styles from './menuButton.module.scss';
-import Link from 'next/link';
+"use client";
+import styles from "./menuButton.module.scss";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 type menuButtonProps = {
-  text:string;
-  href:string;
-}
+  text: string;
+  href: string;
+};
 
-
-export function MenuButton({ text, href } : menuButtonProps) {
+export function MenuButton({ text, href }: menuButtonProps) {
   return (
-      <Link href={href} className={styles.button}>{text}</Link>
-  )
-} 
+    <Link href={href} className={styles.button}>
+      <motion.div
+        whileHover={{ y: 8 }}
+        transition={{ type: "spring", stiffness: 100 }}
+      >
+        {text}
+      </motion.div>
+    </Link>
+  );
+}
