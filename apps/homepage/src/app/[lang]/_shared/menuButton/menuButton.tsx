86520587d7113e64b11d38209af187df8062e1db
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2024-12-24 16:23:13 -0600

    fix: changed the functions in menu button

--- apps/homepage/src/app/[lang]/_shared/menuButton/menuButton.tsx
+++ apps/homepage/src/app/[lang]/_shared/menuButton/menuButton.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { UiIcon, UiIconProps } from "@uireact/icons";
 import { useState } from "react";
+import { useCallback } from "react";
 
 type menuButtonProps = {
   children: React.ReactNode;
@@ -14,8 +15,16 @@ type menuButtonProps = {
 export function MenuButton({ children, href, icon }: menuButtonProps) {
   const [showIcon, setShowIcon] = useState(false);
 
+  const onMouseEnter = useCallback(() => {
+    setShowIcon(true);
+  }, [setShowIcon]);
+
+  const onMouseLeave = useCallback(() => {
+    setShowIcon(false);
+  }, [setShowIcon]);
+
   return (
-    <Link href={href} className={styles.button} onMouseEnter={() => setShowIcon(true)} onMouseLeave={() => setShowIcon(false)}>
+    <Link href={href} className={styles.button} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <motion.div
         whileHover={{ y: 8 }}
         transition={{ type: "spring", stiffness: 100 }}
