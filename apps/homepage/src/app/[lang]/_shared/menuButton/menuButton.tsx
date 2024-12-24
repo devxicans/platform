db80fa94bc17a1703c036c701b0d84cf68497aa9
Luis Eduardo Nava Covarrubias <155337047+iNavac03@users.noreply.github.com>
2024-12-24 16:30:30 -0600

    feat: added animation to menu button (#40)

--- apps/homepage/src/app/[lang]/_shared/menuButton/menuButton.tsx
+++ apps/homepage/src/app/[lang]/_shared/menuButton/menuButton.tsx
@@ -2,19 +2,43 @@
 import styles from "./menuButton.module.scss";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import { UiIcon, UiIconProps } from "@uireact/icons";
+import { useState } from "react";
+import { useCallback } from "react";
 
 type menuButtonProps = {
   children: React.ReactNode;
   href: string;
+  icon: UiIconProps["icon"];
 };
 
-export function MenuButton({ children, href }: menuButtonProps) {
+export function MenuButton({ children, href, icon }: menuButtonProps) {
+  const [showIcon, setShowIcon] = useState(false);
+
+  const onMouseEnter = useCallback(() => {
+    setShowIcon(true);
+  }, [setShowIcon]);
+
+  const onMouseLeave = useCallback(() => {
+    setShowIcon(false);
+  }, [setShowIcon]);
+
   return (
-    <Link href={href} className={styles.button}>
+    <Link href={href} className={styles.button} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       <motion.div
         whileHover={{ y: 8 }}
         transition={{ type: "spring", stiffness: 100 }}
+        className={styles.animation}
       >
+        {showIcon &&
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.4}}
+          >
+          <UiIcon icon={icon} category="tertiary"/>
+        </motion.div>
+        }
         {children}
       </motion.div>
     </Link>
