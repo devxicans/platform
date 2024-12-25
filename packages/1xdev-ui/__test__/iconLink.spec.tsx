7338ca5c48cc6eaca6e930fd2d7d17eeb01f72ad
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2024-12-24 19:32:20 -0600

    test: fixed tests to iconLink component

--- packages/1xdev-ui/__test__/iconLink.spec.tsx
+++ packages/1xdev-ui/__test__/iconLink.spec.tsx
@@ -1,21 +1,21 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent,waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { IconLink } from '../src'
 
-describe('Menu Button Component', () => {
-  test('Should render the button with children', () => {
+describe('Icon Link Component', () => {
+  test('Should render the link with children', () => {
     render(<IconLink href='/' icon='Home'>Home</IconLink>);
     expect(screen.getByRole('link', {name: 'Home'})).toBeInTheDocument;
-    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeVisible();
   })
   
-  test('Should show icon on mouse enter and hide on mouse leave', () => {
+  test('Should show icon on mouse enter and hide on mouse leave', async () => {
     render(<IconLink href="/" icon="Home">Home</IconLink>);
     
-    const button = screen.getByRole('link', { name: 'Home' });
-    fireEvent.mouseEnter(button);
-    expect(screen.getByTestId('Icon')).toBeInTheDocument();
-    fireEvent.mouseLeave(button);
-    expect(screen.queryByTestId('Icon')).toBeNull();
+    const link = screen.getByRole('link', { name: 'Home' });
+    fireEvent.mouseEnter(link);
+    await waitFor(() => expect(screen.getByTestId('Icon')).toBeVisible(), { timeout: 500 });
+    fireEvent.mouseLeave(link);
+    expect(screen.queryByTestId('Icon')).not.toBeInTheDocument();
   });
 })
