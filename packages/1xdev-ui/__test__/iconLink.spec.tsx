1d4bf7aafafbd0b07b004f608922a32d638d8f56
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2024-12-24 19:22:12 -0600

    test: fixed tests to iconLink component

--- packages/1xdev-ui/__test__/iconLink.spec.tsx
+++ packages/1xdev-ui/__test__/iconLink.spec.tsx
@@ -1,16 +1,16 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { MenuButton } from '../src'
+import { IconLink } from '../src'
 
 describe('Menu Button Component', () => {
   test('Should render the button with children', () => {
-    render(<MenuButton href='/' icon='Home'>Home</MenuButton>);
+    render(<IconLink href='/' icon='Home'>Home</IconLink>);
     expect(screen.getByRole('link', {name: 'Home'})).toBeInTheDocument;
     expect(screen.getByText('Home')).toBeInTheDocument();
   })
   
   test('Should show icon on mouse enter and hide on mouse leave', () => {
-    render(<MenuButton href="/" icon="Home">Home</MenuButton>);
+    render(<IconLink href="/" icon="Home">Home</IconLink>);
     
     const button = screen.getByRole('link', { name: 'Home' });
     fireEvent.mouseEnter(button);
