c164f257791ad72cc33f63a75075497f236406ae
AdrifeCova <adrife2000@hotmail.com>
2024-12-24 18:04:27 -0600

    feat: added hero message without localization

--- apps/homepage/src/app/[lang]/page.tsx
+++ apps/homepage/src/app/[lang]/page.tsx
@@ -1,16 +1,58 @@
 'use client'
 import styles from './home.module.css'
 import { useLocalization } from '../../lib/context/loc-context'
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
+const keyWords = [
+  { word: 'engineer', description: 'humans' },
+  { word: 'create', description: '[developers]' },
+  { word: 'code', description: 'artists' },
+];
 
 export default function AppPage() {
-  const loc = useLocalization();
-  const words = loc.heroTextAnimateWords;
-  const descriptions = loc.heroTextAnimateDescriptions;
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % keyWords.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className={styles.main}>
-      <p data-testid="title" className={styles.hero}>{loc.heroText}</p>
+    <div className={styles.hero}>
+      <div className={styles.animation}>
+        <span>We </span>
+        <AnimatePresence initial={false} mode='wait'>
+          <motion.span
+            key={keyWords[index].word}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
+            className={styles.word}
+          >
+            {keyWords[index].word}
+          </motion.span>
+        </AnimatePresence>
+        <span> the future </span>
+      </div>
+      <div className={styles.animation}>
+        <span> while being </span>
+        <AnimatePresence initial={false} mode='wait'>
+          <motion.span
+            key={keyWords[index].description}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5 }}
+            className={styles.description}
+          >
+            {keyWords[index].description}.
+          </motion.span>
+        </AnimatePresence>
+      </div>
     </div>
   )
 }
