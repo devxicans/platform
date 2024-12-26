cc7aa805446bf56f958c929aac38559094f9892a
Luis Eduardo Nava Covarrubias <155337047+iNavac03@users.noreply.github.com>
2024-12-25 21:29:57 -0600

    feat: changed menu buttons to use json file to render (#43)

--- apps/homepage/src/app/[lang]/_shared/header/header.tsx
+++ apps/homepage/src/app/[lang]/_shared/header/header.tsx
@@ -3,6 +3,9 @@ import styles from "./header.module.scss";
 import { IconLink } from "@1xdev/ui";
 import { MobileButton } from "../mobileButton";
 import { Breakpoints, UiViewport } from "@uireact/foundation";
+import buttons from "./buttons.json"
+import { Icon } from "next/dist/lib/metadata/types/metadata-types";
+import { UiIconProps } from "@uireact/icons";
 
 type HeaderProps = {
   loc: {
@@ -17,9 +20,11 @@ export function Header({ loc }: HeaderProps) {
         <div className={styles.main} data-testid="header">
           <h1>@1<span className={styles.spanText}>x</span>Dev</h1>
           <div className={styles.buttonMenu}>
-            <IconLink href="/" icon="Home">{loc.homeLink}</IconLink>
-            <IconLink href="/projects" icon="Layers">{loc.projectsLink}</IconLink>
-            <IconLink href="/contact" icon="Mail">{loc.contactLink}</IconLink>
+            {buttons.map((button, index) => (
+              <IconLink key={index} href={button.href} icon={button.icon as UiIconProps["icon"]}>
+                {loc[button.key]}
+              </IconLink>
+            ))}
           </div>
         </div>
       </UiViewport>
