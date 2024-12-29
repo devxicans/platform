6e7a7e909edb874aaa598619bffa04e7bc3bc2ac
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2024-12-28 21:37:37 -0600

    feat: added active status tu iconLink

--- apps/homepage/src/app/[lang]/_shared/header/header.tsx
+++ apps/homepage/src/app/[lang]/_shared/header/header.tsx
@@ -3,9 +3,9 @@ import styles from "./header.module.scss";
 import { IconLink } from "@1xdev/ui";
 import { MobileButton } from "../mobileButton";
 import { Breakpoints, UiViewport } from "@uireact/foundation";
-import buttons from "./buttons.json"
-import { Icon } from "next/dist/lib/metadata/types/metadata-types";
+import buttons from "./buttons.json";
 import { UiIconProps } from "@uireact/icons";
+import { usePathname } from "next/navigation";
 
 type HeaderProps = {
   loc: {
@@ -14,17 +14,25 @@ type HeaderProps = {
 };
 
 export function Header({ loc }: HeaderProps) {
+  const pathname = usePathname();
+  
   return (
     <>
       <UiViewport criteria="m|l|xl">
         <div className={styles.main} data-testid="header">
           <h1>@1<span className={styles.spanText}>x</span>Dev</h1>
           <div className={styles.buttonMenu}>
-            {buttons.map((button, index) => (
-              <IconLink key={index} href={button.href} icon={button.icon as UiIconProps["icon"]}>
-                {loc[button.key]}
-              </IconLink>
-            ))}
+            {buttons.map((button, index) => {
+              const isActive = 
+                (button.href === "/" && (pathname === "/en" || pathname === "/es")) || 
+                pathname === (`/en${button.href}`) || 
+                pathname === (`/es${button.href}`);
+              return(
+                  <IconLink key={index} href={button.href} icon={button.icon as UiIconProps["icon"]} active={isActive}>
+                    {loc[button.key]}
+                  </IconLink>
+              );
+            })}
           </div>
         </div>
       </UiViewport>
