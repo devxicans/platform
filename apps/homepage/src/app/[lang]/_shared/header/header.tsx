b19f1f684b3014e85f4b56fafc48153afe070138
Luis Eduardo Nava Covarrubias <155337047+iNavac03@users.noreply.github.com>
2024-12-31 22:02:40 -0600

    feat: added active status to iconLink (#45)

--- apps/homepage/src/app/[lang]/_shared/header/header.tsx
+++ apps/homepage/src/app/[lang]/_shared/header/header.tsx
@@ -1,11 +1,8 @@
 "use client";
-import styles from "./header.module.scss";
-import { IconLink } from "@1xdev/ui";
-import { MobileButton } from "../mobileButton";
-import { Breakpoints, UiViewport } from "@uireact/foundation";
-import buttons from "./buttons.json"
-import { Icon } from "next/dist/lib/metadata/types/metadata-types";
-import { UiIconProps } from "@uireact/icons";
+import { UiViewport, Breakpoints } from "@uireact/foundation";
+import { SmallNavbar } from "../smallNavbar";
+import { LargeNavbar } from "../largeNavbar";
+
 
 type HeaderProps = {
   loc: {
@@ -14,29 +11,14 @@ type HeaderProps = {
 };
 
 export function Header({ loc }: HeaderProps) {
+
   return (
     <>
       <UiViewport criteria="m|l|xl">
-        <div className={styles.main} data-testid="header">
-          <h1>@1<span className={styles.spanText}>x</span>Dev</h1>
-          <div className={styles.buttonMenu}>
-            {buttons.map((button, index) => (
-              <IconLink key={index} href={button.href} icon={button.icon as UiIconProps["icon"]}>
-                {loc[button.key]}
-              </IconLink>
-            ))}
-          </div>
-        </div>
+        <LargeNavbar loc={loc}/>
       </UiViewport>
       <UiViewport criteria={Breakpoints.SMALL}>
-        <div className={styles.mobileHeader}>
-          <h1>@1<span className={styles.spanText}>x</span>Dev</h1>
-        </div>
-        <div className={styles.mobileMenu}>
-          <MobileButton icon="Home" href="/"/>
-          <MobileButton icon="Layers" href="/projects"/>
-          <MobileButton icon="Mail" href="/contact"/>
-        </div>
+        <SmallNavbar/>
       </UiViewport>
     </>
   );
