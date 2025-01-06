d7095a7b15f4aef5b130473d7e83bae41c335146
AdrifeCova <adrife2000@hotmail.com>
2025-01-05 19:12:52 -0600

    refactor: added UiText

--- apps/homepage/src/app/[lang]/page.tsx
+++ apps/homepage/src/app/[lang]/page.tsx
@@ -4,6 +4,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import { UiViewport } from '@uireact/foundation';
 import { renderDynamicPhrase } from './utils';
+import { UiText } from '@uireact/text'
 
 const phraseData = {
   phrase: "We {keyWords} the future while being {keyDescriptions}",
@@ -48,39 +49,17 @@ export default function AppPage() {
                   transition={{ duration: 0.5 }}
                   className={styles.dynamic}
                 >
-                  {content.slice(1, -1)}
+                    <UiText size='xlarge' category='tertiary'>
+                      {content.slice(1, -1)}
+                    </UiText>
                 </motion.span>
               </AnimatePresence>
               ) : (
-                <span key={key}>{content}</span>
+                <UiText size='xlarge' key={key}>{content}</UiText>
               )
             )}
           </div>
         </div>
-      </UiViewport>
-      <UiViewport criteria='s|m'>
-        <div className={styles.hero}>
-            <div className={`${styles.wrapper} ${styles.isMobile}`}>
-              {phraseParts.map(({ content, isDynamic, key }) =>
-                isDynamic ? (
-                <AnimatePresence key={key} initial={false} mode="wait">
-                  <motion.span
-                    key={content}
-                    initial={{ opacity: 0, y: 20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: -20 }}
-                    transition={{ duration: 0.5 }}
-                    className={styles.dynamic}
-                  >
-                    {content.slice(1, -1)}
-                  </motion.span>
-                </AnimatePresence>
-                ) : (
-                  <span key={key}>{content} </span>
-                )
-              )}
-            </div>
-          </div>
       </UiViewport>
     </>
   )
