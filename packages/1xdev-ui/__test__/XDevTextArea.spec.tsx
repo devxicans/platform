966c7abd0db0f2b258219a1035fa49986cd7dcdd
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2025-01-12 17:36:53 -0600

    test: fixed XDevInput and XDevTextArea tests

--- packages/1xdev-ui/__test__/XDevTextArea.spec.tsx
+++ packages/1xdev-ui/__test__/XDevTextArea.spec.tsx
@@ -1,10 +1,10 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { CustomTextArea } from '../src';
+import { XDevTextArea } from '../src';
 
-describe('Custom text area component tests', () => {
+describe('XDevTextArea area component tests', () => {
   test('Should render the customTextArea with label and icon', () => {
-    render(<CustomTextArea label="Message" name="message" id="message-input" icon="Send" />);
+    render(<XDevTextArea label="Message" name="message" id="message-input" icon="Send" />);
 
     const input = screen.getByRole('textbox', { name: /message/i });
     const label = screen.getByText(/message/i);
@@ -16,7 +16,7 @@ describe('Custom text area component tests', () => {
   });
 
   test('Should update the text area value on change', async () => {
-    render(<CustomTextArea label="Message" name="message" id="message-input" icon="Send" />);
+    render(<XDevTextArea label="Message" name="message" id="message-input" icon="Send" />);
 
     const input = screen.getByRole('textbox', { name: /message/i });
     fireEvent.change(input, { target: { value: 'hola' } });
