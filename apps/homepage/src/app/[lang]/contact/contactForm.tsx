e0195aaa25631ba44679dcd9f9f803001b9114e5
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2025-01-11 21:37:12 -0600

    fix: added message errors to phone and mail inputs

--- apps/homepage/src/app/[lang]/contact/contactForm.tsx
+++ apps/homepage/src/app/[lang]/contact/contactForm.tsx
@@ -13,8 +13,8 @@ export function ContactForm() {
 
   const schema = {
     name: validator.field("text").present(loc.nameInputError),
-    email: validator.field("email").present(loc.emailInputError1),
-    phone: validator.field("numeric").present(loc.phoneInputError).length(10,12),
+    email: validator.field("email",loc.emailInputError2).present(loc.emailInputError1),
+    phone: validator.field("numeric").present(loc.phoneInputError1).length(10,12,loc.phoneInputError2),
     message: validator
       .field("text")
       .present(loc.messageInputError)
