fd4c4a35790582a642ac1d6486a5178dcc64ff8b
Adrian Covarrubias <145578758+adrife2005@users.noreply.github.com>
2025-01-12 15:48:55 -0600

    feat: added hero message (#41)

---
+++ packages/1xdev-ui/src/ui/animatedHeading/animatedHeading.tsx
@@ -0,0 +1,56 @@
+'use client'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { UiText } from '@uireact/text'
+import styles from "./animatedHeading.module.scss";
+import { renderDynamicPhrase } from "../../utils";
+
+type Props = {
+  phrase: string;
+  keyWords: string[];
+  keyDescriptions: string[];
+  animationTime: number
+}
+
+export function AnimatedHeading({ phrase, keyWords, keyDescriptions, animationTime }: Props) {
+  const { getPhrase, next, splitPhrase } = renderDynamicPhrase({phrase, keyWords, keyDescriptions});
+  const [phrases, setPhrases] = useState(getPhrase());
+  const phraseParts = splitPhrase(phrases);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPhrases(next());
+    }, animationTime);
+    return () => clearInterval(interval);
+  }, [next]);
+
+
+  return (
+    <div data-testid='hero message'>
+    <div className={styles.hero}>
+      <div className={styles.wrapper}>
+        {phraseParts.map(({ content, isDynamic, key }) =>
+          isDynamic ? (
+          <AnimatePresence key={key} initial={false} mode="wait">
+            <motion.span
+              key={content}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.5 }}
+              className={styles.dynamic}
+            >
+                <UiText size='xlarge' category='tertiary'>
+                {content.slice(1, -1)}{' '}
+                </UiText>
+            </motion.span>
+          </AnimatePresence>
+          ) : (
+              <UiText size='xlarge' key={key}>{content}{' '} </UiText>
+          )
+        )}
+      </div>
+    </div>
+  </div>
+  )
+}
