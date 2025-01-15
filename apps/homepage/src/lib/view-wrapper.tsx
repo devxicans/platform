6d7aa5e3d115d24625df9303b5b2c5c6b2b3cebc
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2025-01-15 11:25:06 -0600

    fix: updated view-wrapper

--- apps/homepage/src/lib/view-wrapper.tsx
+++ apps/homepage/src/lib/view-wrapper.tsx
@@ -4,7 +4,7 @@ import { UiView } from "@uireact/view";
 export const ViewWrapper: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => (
-  <div style={{paddingBottom: '70px'}}>
-    <UiView>{children}</UiView>
-  </div>
+  <UiView>
+    <div style={{ paddingBottom: "70px" }}>{children}</div>
+  </UiView>
 );
