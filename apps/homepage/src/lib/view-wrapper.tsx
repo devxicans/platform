ee1018154c2db8d8509d9a1f57d1089ed3c9104b
Luis Eduardo Nava Covarrubias <inavac03@gmail.com>
2025-01-15 11:41:32 -0600

    fix: updated view wrapper styles

--- apps/homepage/src/lib/view-wrapper.tsx
+++ apps/homepage/src/lib/view-wrapper.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { UiView } from "@uireact/view";
 
+const styles = {
+  paddingBottom: "70px"
+}
+
 export const ViewWrapper: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => (
   <UiView>
-    <div style={{ paddingBottom: "70px" }}>{children}</div>
+    <div style={styles}>{children}</div>
   </UiView>
 );
