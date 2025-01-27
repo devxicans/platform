887da5103f3cc8bfc57681393e486b619b5e7936
AdrifeCova <adrife2000@hotmail.com>
2025-01-27 14:41:23 -0600

    refactor: moved data to another file

---
+++ apps/homepage/src/app/[lang]/homeData.ts
@@ -0,0 +1,28 @@
+export const animatedPhraseData = {
+  phrase: "We {keyWords} the future while being {keyDescriptions}",
+  keyWords: ["{engineer}", '{create}', '{code}'],
+  keyDescriptions: ['{humans}', '{developers}', '{artists}']
+}
+
+export const projects = [
+  {
+    id: 0,
+    title: "Lifta",
+    description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos facilis tenetur ipsum obcaecati ducimus! Molestiae in assumenda recusandae accusamus accusantium nesciunt aliquid explicabo est, inventore blanditiis, facilis ipsa tempore fugiat!"
+  },
+  {
+    id: 1,
+    title: "Platform",
+    description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos facilis tenetur ipsum obcaecati ducimus! Molestiae in assumenda recusandae accusamus accusantium nesciunt aliquid explicabo est, inventore blanditiis, facilis ipsa tempore fugiat!"
+  },
+  {
+    id: 2,
+    title: "Yumzo",
+    description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos facilis tenetur ipsum obcaecati ducimus! Molestiae in assumenda recusandae accusamus accusantium nesciunt aliquid explicabo est, inventore blanditiis, facilis ipsa tempore fugiat!"
+  },
+  {
+    id: 3,
+    title: "Save The Budget",
+    description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos facilis tenetur ipsum obcaecati ducimus! Molestiae in assumenda recusandae accusamus accusantium nesciunt aliquid explicabo est, inventore blanditiis, facilis ipsa tempore fugiat!"
+  },
+ ]
--- apps/homepage/src/app/[lang]/page.tsx
+++ apps/homepage/src/app/[lang]/page.tsx
@@ -2,35 +2,7 @@
 import { AnimatedHeading } from "@1xdev/ui"
 import { XDevGallery } from "@1xdev/ui"
 import styles from './hero.module.scss'
-
-const animatedPhraseData = {
-  phrase: "We {keyWords} the future while being {keyDescriptions}",
-  keyWords: ["{engineer}", '{create}', '{code}'],
-  keyDescriptions: ['{humans}', '{developers}', '{artists}']
-}
-
-const projects = [
-  {
-    id: 0,
-    title: "Lifta",
-    description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos facilis tenetur ipsum obcaecati ducimus! Molestiae in assumenda recusandae accusamus accusantium nesciunt aliquid explicabo est, inventore blanditiis, facilis ipsa tempore fugiat!"
-  },
-  {
-    id: 1,
-    title: "Platform",
-    description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos facilis tenetur ipsum obcaecati ducimus! Molestiae in assumenda recusandae accusamus accusantium nesciunt aliquid explicabo est, inventore blanditiis, facilis ipsa tempore fugiat!"
-  },
-  {
-    id: 2,
-    title: "Yumzo",
-    description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos facilis tenetur ipsum obcaecati ducimus! Molestiae in assumenda recusandae accusamus accusantium nesciunt aliquid explicabo est, inventore blanditiis, facilis ipsa tempore fugiat!"
-  },
-  {
-    id: 3,
-    title: "Save The Budget",
-    description : "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos facilis tenetur ipsum obcaecati ducimus! Molestiae in assumenda recusandae accusamus accusantium nesciunt aliquid explicabo est, inventore blanditiis, facilis ipsa tempore fugiat!"
-  },
- ]
+import { animatedPhraseData, projects } from "./homeData"
 
 export default function AppPage() {
   return (
