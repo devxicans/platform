20aca14c7f4c6fead2f6112e8ac63465c6dc6901
Luis Eduardo Nava Covarrubias <155337047+iNavac03@users.noreply.github.com>
2025-02-27 11:45:33 -0600

    feat: added contact form to main page

--- apps/homepage/src/app/[lang]/page.tsx
+++ apps/homepage/src/app/[lang]/page.tsx
@@ -1,14 +1,26 @@
-'use client'
-import { AnimatedHeading } from "@1xdev/ui"
+"use client";
+import { AnimatedHeading } from "@1xdev/ui";
+import { ContactForm } from "./contact/contactForm";
+import styles from "./homePage.module.css"
 
 const animatedPhraseData = {
   phrase: "We {keyWords} the future while being {keyDescriptions}",
-  keyWords: ["{engineer}", '{create}', '{code}'],
-  keyDescriptions: ['{humans}', '{developers}', '{artists}']
-}
+  keyWords: ["{engineer}", "{create}", "{code}"],
+  keyDescriptions: ["{humans}", "{developers}", "{artists}"],
+};
 
 export default function AppPage() {
   return (
-    <AnimatedHeading phrase={animatedPhraseData.phrase } keyWords={animatedPhraseData.keyWords} keyDescriptions={animatedPhraseData.keyDescriptions} animationTime={1000} />
-  )
+    <div className={styles.main}>
+      <AnimatedHeading
+        phrase={animatedPhraseData.phrase}
+        keyWords={animatedPhraseData.keyWords}
+        keyDescriptions={animatedPhraseData.keyDescriptions}
+        animationTime={1000}
+      />
+      <div className={styles.form}>
+      <ContactForm />
+      </div>
+    </div>
+  );
 }
